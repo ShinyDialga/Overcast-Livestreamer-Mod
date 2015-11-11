513216682133866b58cd47c6d70e5dac73de47f3
ShinyDialga45 <shinydialga45@gmail.com>
2015-11-10 19:30:07 -0600

    Update the Mod version

--- src/main/java/com/shinycraft/streamermod/StreamerMod.java
+++ src/main/java/com/shinycraft/streamermod/StreamerMod.java
@@ -20,7 +20,7 @@ import java.io.File;
 public class StreamerMod
 {
     public static final String MODID = "streamermod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static File team1File;
     public static File team2File;
     public static int defaultYLevel = 16;
